ce3678f18df732056b4cd99d2c7da6ddd7a4a4e4
LouiseWhite30 <louise.white30@nhs.net>
2022-05-10 14:00:39 +0100

    setting up folder structure for new project

---
+++ app/views/current/_routes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+const axios = require('axios');
+
+// Add your routes here - above the module.exports line
+
+
+// Left an example route here
+// What do you want to do? - new-application.html
+// router.post('/newApplication', function (req, res) {
+//   var newApplication = req.session.data['new-application']
+//   if (newApplication == "new") {
+//     res.redirect('apply/where-do-you-live')
+//   }
+//   else if (newApplication == "replacement") {
+//     res.redirect('replacement/next-steps')
+//   }
+//   else {
+//     res.redirect('new-application')
+//   }
+// })
+
+module.exports = router
