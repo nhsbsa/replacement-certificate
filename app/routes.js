1aa875b467190f9f568f48daf79bc17ec2e5faae
LouiseWhite30 <louise.white30@nhs.net>
2022-05-19 15:08:58 +0100

    PRC parent and child journey

--- app/routes.js
+++ app/routes.js
@@ -21,7 +21,9 @@ console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subf
 
 // current sprint, remember to add older sprint when adding a new folder!
 // router.use('/current', require('./views/current/_routes'));
+router.use('/current', require('./views/current/_routes'));
 router.use('/current/apply', require('./views/current/apply/_routes'));
+router.use('/current/apply-parent', require('./views/current/apply-parent/_routes'));
 
 
 // Start folder specific routes
